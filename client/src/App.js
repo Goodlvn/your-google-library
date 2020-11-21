47d04f72d3dc7ec438fe348cbb590f8db1c34a0e
JD Martinez <focus4ursoul@gmail.com>
2020-11-21 15:47:40 -0800

    client side react is live

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import React from "react"
+import React from "react";
+import Search from "./pages/Search/Search"
 
 function App() {
   return (
     <div className="App">
+      <Search />
     </div>
   );
 }
