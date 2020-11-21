4a97bb0128220943777f3322c56de86b38997d41
JD Martinez <focus4ursoul@gmail.com>
2020-11-21 15:00:33 -0800

    set up mongoose connection and prep for routes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const path = require("path");
-const PORT = process.env.PORT || 8080;
+
+
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -11,12 +15,16 @@ if(process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"));
 }
 
+app.use(routes)
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", 
+{ useNewUrlParser: true, useUnifiedTopology: true }
+);
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(_dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`server is listening on http://localhost${PORT}`);
+    console.log(`server is listening on http://localhost:${PORT}`);
 })
-
-
