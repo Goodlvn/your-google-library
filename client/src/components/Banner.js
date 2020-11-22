176b084242a9012faf4dcee649e7314731523336
JD Martinez <focus4ursoul@gmail.com>
2020-11-21 16:44:08 -0800

    create banner

---
+++ client/src/components/Banner.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Paper, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        height: "250px",
+        width: "75%",
+        margin: "0 auto",
+        marginTop: "50px",
+        backgroundColor: "#3f51b5"
+    },
+    content: {
+        display: "block",
+        paddingTop: "55px",
+        textAlign: "center",
+        color: "white"
+    }
+}));
+
+export default function Banner() {
+    const classes = useStyles();
+
+    return (
+
+        <Paper elevation={3} className={classes.root}>
+            <div className={classes.content}>
+                <Typography>
+                    <h1>Google Book Search</h1>
+                </Typography>
+                <Typography>
+                    <h3>search for and save books of interest</h3>
+                </Typography>
+            </div>
+        </Paper>
+    );
+}
--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from "../../components/NavBar";
+import Banner from "../../components/Banner"
 
 export default function Search() {
     return (
         <div>
             <NavBar/>
+            <Banner />
         </div>
     )
 }
