bdecc8e2637403b36e4cbada542c497baec6efee
JD Martinez <focus4ursoul@gmail.com>
2020-11-21 18:06:21 -0800

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
 import React from "react";
-import Search from "./pages/Search/Search"
+import Search from "./pages/Search/Search";
+import Saved from "./pages/Saved/Saved";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {
   ThemeProvider,
   createMuiTheme
 } from "@material-ui/core/styles"
 
 const customTheme = createMuiTheme({
-  palette:{
+  palette: {
     type: "dark"
   }
 });
 
 function App() {
   return (
-    <ThemeProvider theme={customTheme}>
-      <div className="App">
-        <Search />
-      </div>
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={customTheme}>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+          </Switch>
+        </div>
+      </ThemeProvider>
+    </Router>
   );
 }
 
