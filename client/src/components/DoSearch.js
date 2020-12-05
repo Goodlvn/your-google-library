1d9085c2037eae1ede9a25405561be5074ab3f37
JD Martinez <focus4ursoul@gmail.com>
2020-12-04 22:28:15 -0800

    return array of book results

--- client/src/components/DoSearch.js
+++ client/src/components/DoSearch.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { TextField, Button, Grid } from '@material-ui/core';
+import { useBookContext } from '../utils/BookContext';
+import API from "../utils/API";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,23 +15,37 @@ const useStyles = makeStyles((theme) => ({
 
 export default function DoSearch() {
   const classes = useStyles();
+  const { dispatch } = useBookContext();
   const [title, setTitle] = useState("");
 
   const handleChange = e => {
     const { value } = e.target;
     setTitle(value);
-    console.log(title);
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const { data } = await API.searchTitle(title);
+    let bookResults;
+    if (data) {
+      bookResults = data.map(results => {
+        const { volumeInfo } = results;
+        return volumeInfo;
+      })
+    }
+
+    console.log(bookResults);
   }
 
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <Grid container style={{margin: "0 auto"}}>
+    <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+      <Grid container style={{ margin: "0 auto" }}>
         <Grid item xs={12}>
-          <TextField id="filled-basic" label="Book" variant="filled" onChange={handleChange}/>
+          <TextField id="filled-basic" label="Book" variant="filled" onChange={handleChange} />
         </Grid>
         <Grid item xs={12}>
           <Button
-            style={{marginTop: "15px"}}
+            style={{ marginTop: "15px" }}
             // disabled={title.trim().length === 0}
             type="submit"
             variant="contained"
