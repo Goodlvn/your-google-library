f3561ba2366b8a07b8650ee10e2a4b1b54050110
JD Martinez <focus4ursoul@gmail.com>
2020-12-06 20:13:05 -0800

    save book to database

--- client/src/components/BookDisplay.js
+++ client/src/components/BookDisplay.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Paper, Typography, withTheme } from '@material-ui/core';
+import SavedButton from "./SavedButton";
+import API from "../utils/API";
+import { useBookContext } from "../utils/BookContext";
+import Actions from "../utils/Actions";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -11,7 +15,8 @@ const useStyles = makeStyles((theme) => ({
         marginTop: "50px",
         backgroundColor: "#818B94",
         marginBottom: "75px",
-        padding: "20px"
+        padding: "20px",
+        position: "relative"
     },
     content: {
         display: "flex",
@@ -21,28 +26,70 @@ const useStyles = makeStyles((theme) => ({
         margin: 0,
         paddingTop: "15px",
         paddingLeft: "15px",
+    },
+    saved: {
+        float: "right"
     }
 }));
 
-export default function BookDisplay({ image, title, description }) {
+export default function BookDisplay({ image, title, authors, description, link, bookID, split }) {
     const classes = useStyles();
+    const { dispatch } = useBookContext();
+
+    let authorsString;
+
+    if (authors) {
+        authors.forEach((author, index) => {
+
+            index === 0 ? authorsString = author :
+                authorsString += ", " + author
+        })
+    }
+
+    async function saveBook(e) {
+
+        let addBook = {
+            bookID,
+            title,
+            authors,
+            description,
+            image,
+            link
+        }
+
+        const { data } = await API.saveBook(addBook);
+
+        if (data) {
+            dispatch({ type: Actions.SAVE_BOOK, payload: data })
+        }
+    }
 
     return (
 
         <Paper elevation={3} className={classes.root}>
+            <SavedButton className={classes.saved} onClick={saveBook} split={split} />
             <div className={classes.content}>
-                {image ? <img src={image} alt="book cover" /> : <img src="https://via.placeholder.com/150" alt="book cover" />}
-                <div>
-                    <Typography>
-                        {title}
-                    </Typography>
+                {image ? <img style={{ minHeight: "198px", minWidth: "128px" }} src={image} alt="book cover" /> : <img src="https://via.placeholder.com/150" alt="book cover" />}
+                <div style={{ marginLeft: "15px" }}>
+                    <div style={{ display: "flex", alignItems: "center", flexWrap: "wrap" }}>
+                        <Typography variant="h5" component="h2">
+                            {title}
+                        </Typography>
+                        <Typography variant="h5" component="h2" style={{ marginLeft: "15px", marginRight: "15px" }} >
+                            •
+                         </Typography>
+                        <Typography style={{ fontStyle: "italic" }} >
+                            {authorsString === undefined ? "No Author Information is available" : authorsString}
+                        </Typography>
+                    </div>
+
 
                     <Typography>
-                        {description ? description.slice(0, 250).concat('...') : "no description availible"}
+                        {description ? description.slice(0, 250).concat('...') : "no description available"}
                     </Typography>
 
                 </div>
             </div>
-        </Paper>
+        </Paper >
     );
 }
