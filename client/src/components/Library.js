e84943eed720a33f8e5c2a3d08056c6e55cf1593
JD Martinez <focus4ursoul@gmail.com>
2020-12-07 12:12:31 -0800

    add library display

--- client/src/components/Library.js
+++ client/src/components/Library.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Paper, Typography, withTheme } from '@material-ui/core';
+import { Paper, Typography } from '@material-ui/core';
+import { useBookContext } from "../utils/BookContext";
+import BookDisplay from "./BookDisplay";
+
+import API from "../utils/API";
+import Actions from "../utils/Actions";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        height: "350px",
+        height: "auto",
+        minHeight: "250px",
         width: "75%",
         margin: "0 auto",
         marginTop: "50px",
         backgroundColor: "#3f51b5",
-        marginBottom: "75px"
+        marginBottom: "75px",
+        paddingBottom: "25px"
     },
     content: {
         display: "block",
@@ -24,17 +31,47 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Library() {
+    const { state: { savedBooks }, dispatch } = useBookContext();
     const classes = useStyles();
+    useEffect(() => {
+        loadSavedBooks();
+    }, [])
+
+    async function loadSavedBooks() {
+        const { data } = await API.getBooks();
+        dispatch({ type: Actions.GET_SAVED_BOOKS, payload: data });
+    }
+
+    const displayBookResults = savedBooks.map((book, index) => {
+        //otherwise the split is defaulted into flase
+        let split = false;
+        //if the book id matches the a book with the same ID in state then we have to change the split to true
+
+        savedBooks.forEach(savedBook => {
+            if (savedBook.bookID === book.bookID) {
+                split = true;
+            }
+        })
+
+        return <BookDisplay
+            key={index}
+            bookID={book.bookID}
+            image={book.image}
+            title={book.title}
+            authors={book.authors}
+            description={book.description}
+            link={book.previewLink}
+            split={split}
+        />
+    })
 
     return (
-
         <Paper elevation={3} className={classes.root}>
             <div className={classes.content}>
-                <h3 className={classes.label}>
-                    <Typography>
-                        Saved Books
-                    </Typography>
-                </h3>
+                <Typography className={classes.label}>
+                    Saved Books
+                </Typography>
+                {displayBookResults}
             </div>
         </Paper>
     );
