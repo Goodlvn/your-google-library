f7ee1be25297d2e5c064b6f28494b4ac4f20afa9
JD Martinez <focus4ursoul@gmail.com>
2020-12-08 10:41:14 -0800

    change root https call to buil

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use("/api", apiRoutes);
 app.use("/google", google);
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
